404bc83c5ff9c2a2a60b2256b23410382b80ebf7
keroles Rozek <kerolesrozek2@gmail>
2025-04-05 14:16:39 +0200

    add firebase for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_dashboard/core/app_routes.dart';
+import 'package:fruits_dashboard/firebase_options.dart';
 
-void main() {
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FruitsDashBoard());
 }
 class FruitsDashBoard extends StatelessWidget {
